febe35cd8bf9600d96b90de954eb1aa22a2cc7c0
Raza Kazmi <razakazmi@Razas-MacBook-Pro-2.local>
2024-06-18 22:14:19 +0500

    update files

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,6 @@ import { setAuthedUser } from '../actions/authedUser';
 import { useNavigate } from 'react-router-dom';
 import './navbar.css';
 
-
 const Navbar = ({ authedUser, dispatch, users }) => {
   const navigate = useNavigate();
 
@@ -17,16 +16,18 @@ const Navbar = ({ authedUser, dispatch, users }) => {
   return (
     <nav>
       <ul>
-        <li><Link to="/home">Home</Link></li>
-        <li><Link to="/leaderboard">Leaderboard</Link></li>
-        <li><Link to="/add">Create Poll</Link></li>
+        <div className="nav-left">
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/leaderboard">Leaderboard</Link></li>
+          <li><Link to="/add">Create Poll</Link></li>
+        </div>
         {authedUser && (
-          <>
+          <div className="nav-right">
             <li>
               <span>Hello, {users[authedUser].name}</span>
             </li>
             <li><button onClick={handleLogout}>Logout</button></li>
-          </>
+          </div>
         )}
       </ul>
     </nav>
