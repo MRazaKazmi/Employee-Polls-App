c57689ad338e537fb63bb1fb605acab7e95ef8af
Raza Kazmi <razakazmi@Razas-MacBook-Pro-2.local>
2024-06-18 16:28:26 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { handleInitialData } from './actions/shared'
+import { useEffect } from "react";
+import { connect } from "react-redux";
 
-function App() {
+
+
+const App = (props) => {
+  useEffect(() => {
+    props.dispatch(handleInitialData());
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World!
     </div>
   );
 }
 
-export default App;
+export default connect()(App);
+
